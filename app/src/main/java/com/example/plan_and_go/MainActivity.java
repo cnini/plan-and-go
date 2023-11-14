5a5eb8936248efa05d2f06bcc64b1c8b0967f953
Maxime <maxime.sausset@hotmail.fr>
2023-11-14 11:13:42 +0100

    Test 2

--- app/src/main/java/com/example/plan_and_go/MainActivity.java
+++ app/src/main/java/com/example/plan_and_go/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Test
+        // Test 2
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
