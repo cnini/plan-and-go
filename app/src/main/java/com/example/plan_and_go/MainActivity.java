91f97240c8dd6c5b39a54470c6640613a0a64030
cnini <cynthia-zoely-nirina.rabemanantsoa@estiam.com>
2023-11-14 14:03:56 +0100

    front : home page with input (editTripEnd)

--- app/src/main/java/com/example/plan_and_go/MainActivity.java
+++ app/src/main/java/com/example/plan_and_go/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.plan_and_go;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.plan_and_go.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.trips) {
+                replaceFragment(new TripsFragment());
+            } else if (item.getItemId() == R.id.myAccount) {
+                replaceFragment(new MyAccountFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
     }
 }
